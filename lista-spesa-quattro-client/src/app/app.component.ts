cdaaf435a86576e700fdaf5c04178ead86e529c3
Alberto Martinez Montero <martinezmontero.al@gmail.com>
2021-03-03 13:21:04 +0100

    Commit 4

--- lista-spesa-quattro-client/src/app/app.component.ts
+++ lista-spesa-quattro-client/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RichiestaDto } from './richiesta-dto';
+import { RispostaDto } from './risposta-dto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista-spesa-quattro-client';
+  lista: String[] = [];
+  nome: String = "";
+
+  constructor(private http: HttpClient) { }
+
+  aggiungi() {
+    let dto: RichiestaDto = new RichiestaDto();
+     dto.nome = this.nome;
+     this.lista.push(this.nome)
+   
+    let oss: Observable<RispostaDto> = this.http.post<RispostaDto>("http://localhost:8080/aggiungi", dto);
+    oss.subscribe(s => this.nome = s.nome);
+    this.nome=""; 
+
+  }
+  
+  cancella(lista) {
+    let dto: RichiestaDto = new RichiestaDto();
+     dto.nome = this.nome;
+     this.lista.splice(lista,this.lista.length);
+   
+    let oss: Observable<RispostaDto> = this.http.post<RispostaDto>("http://localhost:8080/cancella", dto);
+    oss.subscribe(s => this.nome = s.nome); 
+
+  }
+
 }
